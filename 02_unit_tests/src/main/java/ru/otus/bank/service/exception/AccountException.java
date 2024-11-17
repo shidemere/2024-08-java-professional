3ae7fde5db05f991856ce94d3e433d1952c82e24
prth <tihopadaetsneg@gmail.com>
2024-11-17 14:57:41 +0300

    removing homework02 module after invalid pushing

--- 02_unit_tests/src/main/java/ru/otus/bank/service/exception/AccountException.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.bank.service.exception;
-
-public class AccountException extends RuntimeException {
-
-    public AccountException(String message) {
-        super(message);
-    }
-}
