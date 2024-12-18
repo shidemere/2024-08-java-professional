3947e4c00ab90fa3a8ec8500605c895dc156a3a0
prth <tihopadaetsneg@gmail.com>
2024-12-19 00:18:06 +0300

    [Homework 06] minor fix by comments

--- 06_Patterns/src/main/java/ru/otus/hw/config/CustomDataSource.java
+++ 06_Patterns/src/main/java/ru/otus/hw/config/CustomDataSource.java
@@ -5,20 +5,28 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CustomDataSource {
-    private static volatile CustomDataSource instance;
+
+    private static final CustomDataSource INSTANCE = new CustomDataSource();
 
     public static CustomDataSource getInstance() {
-        CustomDataSource localInstance = instance;
-        if (localInstance == null) {
-            synchronized (CustomDataSource.class) {
-                localInstance = instance;
-                if (localInstance == null) {
-                    instance = localInstance = new CustomDataSource();
-                }
-            }
-        }
-        return localInstance;
+        return INSTANCE;
     }
+
+    // Реализация Singleton через DoubleChecking. Проверяющий счёл что это излишне, поэтому сделал способ выше
+//    private static volatile CustomDataSource instance;
+//
+//    public static CustomDataSource getInstance() {
+//        CustomDataSource localInstance = instance;
+//        if (localInstance == null) {
+//            synchronized (CustomDataSource.class) {
+//                localInstance = instance;
+//                if (localInstance == null) {
+//                    instance = localInstance = new CustomDataSource();
+//                }
+//            }
+//        }
+//        return localInstance;
+//    }
 
     public void beginTransaction() {
         System.out.println("Начало транзакции");
