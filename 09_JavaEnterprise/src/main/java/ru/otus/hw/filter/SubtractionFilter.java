1bd4454f24973f616302490dca79493396dfe320
prth <tihopadaetsneg@gmail.com>
2025-01-02 02:21:11 +0300

    [Homework 09] add homework

---
+++ 09_JavaEnterprise/src/main/java/ru/otus/hw/filter/SubtractionFilter.java
@@ -0,0 +1,23 @@
+package ru.otus.hw.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.hw.servlet.SubtractionServlet;
+
+import java.io.IOException;
+
+@WebFilter(value = "/subtract")
+public class SubtractionFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(SubtractionFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String firstValue = servletRequest.getParameter("first");
+        String secondValue = servletRequest.getParameter("second");
+        logger.info("Register request in {} with values {{}, {}}", SubtractionServlet.class.getSimpleName(), firstValue, secondValue);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
---
+++ 09_JavaEnterprise/src/main/java/ru/otus/hw/servlet/AddingServlet.java
@@ -0,0 +1,36 @@
+package ru.otus.hw.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "AddServlet", urlPatterns = "/add")
+public class AddingServlet extends HttpServlet {
+
+    // http://localhost:8080/09_JavaEnterprise/calc?first=10&second=100
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        int first = Integer.parseInt(req.getParameter("first"));
+        int second = Integer.parseInt(req.getParameter("second"));
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        String out = String.format("""
+                <!DOCTYPE html>
+                <html lang="en">
+                  <head>
+                    <meta charset="UTF-8">
+                    <title>Result</title>
+                  </head>
+                  <body>
+                        <h1>%d</h1>
+                  </body>
+                </html>
+                """, first + second);
+        writer.println(out);
+        writer.close();
+    }
+}
